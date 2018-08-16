0284bca497f949f4b87e7b0c160237c7415b2a69
hkn10004 <hkn10004@naver.com>
2018-08-16 18:18:05 +0900

    ChrStatusInfo 클래스 추가

---
+++ FlowerEngine/Application/ChrStatusInfo.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ChrStatusInfo.h"
+
+
+ChrStatusInfo::ChrStatusInfo()
+{
+}
+
+
+ChrStatusInfo::~ChrStatusInfo()
+{
+}
---
+++ FlowerEngine/Application/ChrStatusInfo.h
@@ -0,0 +1,8 @@
+#pragma once
+class ChrStatusInfo
+{
+public:
+	ChrStatusInfo();
+	virtual ~ChrStatusInfo();
+};
+
