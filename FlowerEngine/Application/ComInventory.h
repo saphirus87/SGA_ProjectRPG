501c93f62087ad8d58e3abf2987fe256cf3bb985
saphirus87 <saphirus87@gmail.com>
2018-08-29 17:22:16 +0900

    인벤토리 기능 추가
    아이템 이동 및 교체

--- FlowerEngine/Application/ComInventory.h
+++ FlowerEngine/Application/ComInventory.h
@@ -13,7 +13,7 @@ public:
 	virtual void Awake() override;
 	virtual void Update() override;
 	virtual void Render() override;
-	
+
 	// UIButtonDelegate을(를) 통해 상속됨
 	virtual void OnClick(UIButton * pSender) override;
 	virtual void OnPress(UIButton * pSender) override;
@@ -23,11 +23,15 @@ public:
 	pair<CString, UINT> DeleteItem(int InvenNum);
 	bool FindItem(CString szItemName, int& StartIndex);
 	bool FindEmptySlot(int& StartIndex);
+	bool PickItem(int InvenNum);
+	void UpdateIcons();
 
 	bool SetInvenSize(UINT InvenSize);
 
 private:
 	vector<pair<CString, UINT>> m_vecItem;
 	UINT m_InvenSize;
+
+	pair<CString, UINT> m_PickedItem;
 };
 
